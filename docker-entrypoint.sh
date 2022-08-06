8c523a88db27e321c7b22ae484cbebd8c88869d9
Huahai Yang <huahai.yang@gmail.com>
2022-08-05 17:14:06 -0700

    remove warnings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,13 +7,21 @@ if [ "$1" = 'supervisord' ]; then
   cat <<EOF > /etc/supervisor/conf.d/supervisord.conf
 [supervisord]
 nodaemon=true
+user=root
 loglevel=info
 
+[unix_http_server]
+file = /var/run/supervisord.sock
+chmod = 0700
+username = dummy
+password = dummy
+
 [program:datalevin]
-command=java -jar /opt/datalevin.jar serv -v -r $DATALEVIN_ROOT -p $DATALEVIN_PORT
+command=java --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED -jar /opt/datalevin.jar serv -v -r $DATALEVIN_ROOT -p $DATALEVIN_PORT
 stdout_logfile=/dev/stdout
 stdout_logfile_maxbytes=0
 redirect_stderr=true
+user=root
 
 EOF
 
